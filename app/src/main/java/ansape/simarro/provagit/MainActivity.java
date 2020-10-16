3f7e0983398fb9ab1b14caf8058402f08f350176
lliurex <xeruilL>
2020-10-16 13:27:39 +0200

    He afegit un botó

---
+++ app/src/main/java/ansape/simarro/provagit/MainActivity.java
@@ -0,0 +1,14 @@
+package ansape.simarro.provagit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
